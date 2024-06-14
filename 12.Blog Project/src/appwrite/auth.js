580f8cf507182c515b8f38b88a1a7cd257f28a73
Harsh <wharsh28@gmail.com>
2024-06-14 17:16:39 +0530

    Authentication

---
+++ 12.Blog Project/src/appwrite/auth.js
@@ -0,0 +1,59 @@
+import conf from "../conf/conf";
+import {Client,Account,ID} from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl) // Your API Endpoint
+            .setProject(conf.appwriteProjectId);// Your project ID
+        this.account = new Account(this.client)
+    }
+    async createAccount({email,password,name}){
+        try {
+           const UserAccount=   await this.account.create(ID.unique(), email,password,name); 
+           
+           if (UserAccount) {
+            //login to account
+            return this.login({email,password});
+           } else {
+             return UserAccount;
+           }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email,password}){
+        try {
+           return await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error
+        }
+        return null ;
+    }
+
+    async logOut(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error
+        }
+    }
+   
+}
+
+const authService = new AuthService();
+
+export default AuthService
